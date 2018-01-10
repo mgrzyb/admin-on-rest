8e877fa7d9f536c186aa845fb7901084567520ec
gildas <gildas.garcia@gmail.com>
2018-01-10 11:42:00 +0100

    Debug

--- e2e/chromeDriver.js
+++ e2e/chromeDriver.js
@@ -5,7 +5,12 @@ import { Options } from 'selenium-webdriver/chrome';
 const options = new Options();
 options.addArguments('--start-maximized');
 
-export default new webdriver.Builder()
+const driver = new webdriver.Builder()
     .forBrowser('chrome')
     .setChromeOptions(options)
-    .build();
+    .build()
+    .catch(error => {
+        console.error(error);
+    });
+
+export default driver;
