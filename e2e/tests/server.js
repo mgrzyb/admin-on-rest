9a874e8312f6923709949756f8726c1c4e88de29
gildas <gildas.garcia@gmail.com>
2018-01-10 10:13:25 +0100

    Logs

--- e2e/tests/server.js
+++ e2e/tests/server.js
@@ -8,18 +8,22 @@ before(
     () =>
         new Promise((resolve, reject) => {
             const server = express();
+            console.log('Initialize express');
             server.use(
                 '/',
                 express.static(
                     path.join(__dirname, '../../packages/ra-example')
                 )
             );
+            console.log('Configure express');
 
             listeningServer = server.listen(8083, err => {
                 if (err) {
+                    console.err(err);
                     return reject(err);
                 }
 
+                console.log('Express ready on 8083');
                 resolve();
             });
         })
